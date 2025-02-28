77208c304e371f4e270633cc535d1fb24d312fc8
Aviral Jain <aviral2853@gmail.com>
2025-02-28 23:13:03 +0530

    removed footer from dashboard

--- src/app/(otherPages)/layout.tsx
+++ src/app/(otherPages)/layout.tsx
@@ -10,7 +10,7 @@ export default async function RootLayout({ children }: RootLayoutProps) {
     <div className="">
       <Navbar />
       {children}
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 }
